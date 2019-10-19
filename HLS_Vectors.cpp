9db4ac1a906066bf080fe45007ffe441069da757
Simone Bologna <smnbologna@gmail.com>
2019-10-19 19:14:14 +0100

    Removed hls constructs

--- HLS_Vectors.cpp
+++ HLS_Vectors.cpp
@@ -10,8 +10,6 @@ void hls_vectorSum(const Vector inVector1,
               const Vector inVector2,
               Vector outVectorSum)
 {
-  // inlining the function code to enable further optimisation and save latency and resources
-  #pragma HLS inline
   sumLoop: for (unsigned int x = 0 ; x < ARRAY_SIZE ; x++)
   {
     outVectorSum[x] = inVector1[x] + inVector2[x];
@@ -22,8 +20,6 @@ void hls_vectorSum(const Vector inVector1,
 ap_int<16> hls_vectorScalarProduct(const Vector inVector1,
               const Vector inVector2)
 {
-  // inlining the function code to enable further optimisation and save latency and resources
-  #pragma HLS inline
   ap_int<16> lProduct = 0;
   productLoop: for (unsigned int x = 0 ; x < ARRAY_SIZE ; x++)
   {
@@ -39,14 +35,6 @@ void hls_vectorOperations(
               ap_int<16> & outProduct
              )
 {
-
-  //partition the memory in smaller areas, to increase memory throughput and enable parallel access
-  #pragma HLS array_partition variable=inVector1
-  #pragma HLS array_partition variable=inVector2
-  #pragma HLS array_partition variable=outVectorSum
-  //maximising throughput by pipelining the function code, and unrolling and pipelining any underlying loop
-  #pragma HLS pipeline
-
   // __SYNTHESIS__ is defined only during synthesis
   // wrap code you do not want to be synthesised in ifndef __SYNTHESIS__
   // conversely, wrap code you want to be synthetised, but not compiled in ifdef __SYNTHESIS__
