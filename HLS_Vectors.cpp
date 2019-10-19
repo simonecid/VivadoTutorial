5dbc50239b7c88970ff7d156393c4fe284bed511
Simone Bologna <smnbologna@gmail.com>
2019-10-19 19:10:07 +0100

    Removed template

--- HLS_Vectors.cpp
+++ HLS_Vectors.cpp
@@ -6,15 +6,13 @@
 #endif
 
 
-// Template function to sum two vectors of any size
-template<class TSrcVector, class TDestVector, unsigned int vectorSize>
-void hls_vectorSum(TSrcVector inVector1,
-              TSrcVector inVector2,
-              TDestVector outVectorSum)
+void hls_vectorSum(const Vector inVector1,
+              const Vector inVector2,
+              Vector outVectorSum)
 {
   // inlining the function code to enable further optimisation and save latency and resources
   #pragma HLS inline
-  sumLoop: for (unsigned int x = 0 ; x < vectorSize ; x++)
+  sumLoop: for (unsigned int x = 0 ; x < ARRAY_SIZE ; x++)
   {
     outVectorSum[x] = inVector1[x] + inVector2[x];
   }
@@ -60,5 +58,5 @@ void hls_vectorOperations(
   #ifndef __SYNTHESIS__ 
   std::cout << "Summin' stuff up!" << std::endl;
   #endif
-  hls_vectorSum<const Vector, Vector, ARRAY_SIZE>(inVector1, inVector2, outVectorSum);
+  hls_vectorSum(inVector1, inVector2, outVectorSum);
 }
--- HLS_Vectors.h
+++ HLS_Vectors.h
@@ -3,10 +3,9 @@
 
 typedef ap_int<16> Vector[ARRAY_SIZE];
 
-template<class TVector, unsigned int vectorSize>
-void hls_vectorSum(const TVector inVector1,
-              const TVector inVector2,
-              TVector outVectorSum);
+void hls_vectorSum(const Vector inVector1,
+              const Vector inVector2,
+              Vector outVectorSum);
 
 ap_int<16> hls_vectorScalarProduct(const Vector inVector1,
               const Vector inVector2);
